85764471ddcf35b150b361dba7c0d03a62d52a04
Yuuta Tanaka <tanakayuuta0321@gmail.com>
2017-12-03 04:03:48 +0000

    Install the devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,26 @@
 module ApplicationHelper
   # ページごとの完全なタイトルを返します。
   def full_title(page_title)
-    base_title = "サンプルアプリ"
+    base_title = "あつまて（仮）"
     if page_title.empty?
       base_title
     else
       "#{page_title} | #{base_title}"
     end
   end
+  
+   def bootstrap_class_for(flash_type)
+    case flash_type
+      when :success
+        "alert-success"
+      when :error
+        "alert-danger"
+      when :alert
+        "alert-warning"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+   end
 end
