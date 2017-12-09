933a9db631a34cee9f122fc0af2e853ffbc7c0e1
Yuuta Tanaka <tanakayuuta0321@gmail.com>
2017-12-09 12:12:10 +0000

    Finish user index action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ OriginApp::Application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => "registrations"
   }
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
   root  'static_pages#home'
   
   get "users/show"
