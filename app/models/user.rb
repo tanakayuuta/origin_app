cba88480f9b1698b247ccc2ddac3183a9bd1e484
Yuuta Tanaka <tanakayuuta0321@gmail.com>
2017-12-15 10:42:10 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   
-  
+  has_many :microposts, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
   validates :name,  presence: true, length: { maximum: 50 }
   
+    def feed
+    Micropost.where("user_id = ?", id)
+    end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ OriginApp::Application.routes.draw do
     :registrations => "registrations"
   }
   resources :users, only: [:show, :index, :destroy]
+  resources :microposts, only: [:create, :destroy]
   root  'static_pages#home'
   
   get "users/show"
