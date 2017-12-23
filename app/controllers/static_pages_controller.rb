6a90230dde48de0a004acfda5fd49073c6f04923
Yuuta Tanaka <tanakayuuta0321@gmail.com>
2017-12-23 13:40:41 +0000

    toppage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,6 +10,10 @@ class StaticPagesController < ApplicationController
   end
   
   def mypage
+    if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
   
   def contact
